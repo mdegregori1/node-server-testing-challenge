7cf1e7a5585d7439cf62d0a848091ddfe00b8445
Mauricio Degregori <degregori2@gmail.com>
2019-12-19 18:53:25 -0600

    crud functionality working (no tests yet)

--- cars/cars-model.js
+++ cars/cars-model.js
@@ -1,25 +1,34 @@
 const db = require('../database/dbConfig.js');
 
 module.exports = {
-//   add,
-//   find,
+  add,
+  find,
+  remove
 //   findBy,
 //   findById,
 };
 
-// function find() {
-//   return db('users').select('id', 'username', 'password', 'department');
-// }
+function find() {
+  return db('cars').select('id', 'make', 'model', 'year');
+}
 
 // function findBy(filter) {
 //   return db('users').where(filter);
 // }
 
-// async function add(user) {
-//   const [id] = await db('users').insert(user);
+function add(car) {
+  return db('cars').insert(car)
+  .then(ids => {
+    return db('cars').where({id: ids[0]}).first()
+  })
+    
+}
 
-//   return findById(id);
-// }
+function remove(id){
+  return db('cars')
+  .where({id})
+  .del()
+}	
 
 // function findById(id) {
 //   return db('users')
--- cars/cars-router.js
+++ cars/cars-router.js
@@ -2,6 +2,44 @@ const router = require("express").Router();
 
 const Cars = require("./cars-model.js");
 
+router.post('/', (req, res) => {
+    let car = req.body;
 
+    Cars.add(car)
+    .then(cars => {
+        res.status(201).json(cars)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+  });
+
+router.get('/', (req,res) => {
+
+    Cars.find()
+    .then(cars => {
+        res.status(200).json(cars)
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json({message: "error retrieving cars list"})
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    Cars.remove(id)
+    .then(id => {
+        if(id){
+            res.status(204).json({message: "car erased successfully"})
+        } else {
+            res.status(404).json({message: "could not find car with the given id"})
+        }
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ErrorMessage: "failed from server"})
+    })
+})
 
 module.exports = router;
